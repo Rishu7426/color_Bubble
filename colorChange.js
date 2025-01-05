76c37bade47f42c99d8509eb5486bf2ac9124ba0
Rishu Raj <107173969+Rishu7426@users.noreply.github.com>
2025-01-06 00:24:50 +0530

    Update colorChange.js
    Changed Arrow( )=> function , to function declaration( )

--- colorChange.js
+++ colorChange.js
@@ -1,63 +1,78 @@
 
 let interval = null, i = 0;
 let boxes = document.getElementsByClassName("box");
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
 
+//iterating over all 60 boxes and filling random-Gradient-colors one-by-one
 function color() {
     for (i; i < boxes.length; i++) {
         boxes[i].style.background = gradient();
         boxes[i].style.boxShadow = shadow();
-       i++;
+        i++;
         break;
     }
 }
-let start = document.getElementById("start");
-start.addEventListener('click', () => {
+//Start color change function
+function startColor() {
     if (!interval) {
-        interval = setInterval(color, 500);
-        start.disabled = true;
+        interval = setInterval(color, 50);
+        start.disabled = true;//disabling start button after one click
     }
-});
-let stop = document.getElementById("stop");
-stop.addEventListener('click', () => {
+}
+//calling startColor function on STOP button click
+start.addEventListener('click', startColor);
+
+//stopColorChange Function
+function stopColorChange() {
     clearInterval(interval);
     interval = null;
-    start.disabled = false;
-});
-let reset = document.getElementById("reset");
-reset.addEventListener('click', () => {
+    start.disabled = false;//Enabling start button after STOP button click
+}
+
+//calling stopColorChange function on STOP button click
+stop.addEventListener('click', stopColorChange);
+
+//Reset color Function
+function resetColor() {
     clearInterval(interval);
     interval = null;
-    i = 0;
+    i = 0;// reset the current iteration index of the color function after the RESET button is clicked.This ensures that when the Start button is clicked again after resetting,the color changes begin from the first box (index 0) rather than continuing from where the last iteration stopped.
+
     for (let j = 0; j < boxes.length; j++) {
-        boxes[j].style.background = "white";
+        boxes[j].style.background = "transparent";
         boxes[j].style.boxShadow = "1px 4px 2px rgb(0, 0, 0)";
     }
     start.disabled = false;
-});
+}
+//calling resetColor function on STOP button click
+reset.addEventListener('click', resetColor);
 
 
- //function to generate random gradient colors
-  function gradient() {
-    const color1 = randomRgbaString();
-    const color2 = randomRgbaString();
-    const color3 = randomRgbaString();
+
+
+//function to generate random gradient colors
+function gradient() {
+    const color1 = randomRgbString();
+    const color2 = randomRgbString();
+    const color3 = randomRgbString();
     return `linear-gradient(45deg, ${color1}, ${color2},${color3})`;
 }
 
 //to generate shadow
-function shadow(){
+function shadow() {
     return `inset 5px 8px 5px black`;
 }
-  
+
 //to generate Random colors in javascript
-function randomRgbaString () {
+function randomRgbString() {
     let r = Math.floor(Math.random() * 255)
     let g = Math.floor(Math.random() * 255)
     let b = Math.floor(Math.random() * 255)
-    return `rgba(${r},${g},${b})`
-  }
-  
+    return `rgb(${r},${g},${b})`
+}
 
- 
 
- 
+
+
